d31d1799b30025b2e216b4536ee6a30cb5732b17
xentrixgitlab <gitlabxentrix@xentrixstudios.com>
2025-03-04 21:21:18 +0530

    wait time added
    to load all the GPU elements in the scene file wait time is added before taking the playblast

---
+++ Config.py
@@ -0,0 +1,6 @@
+#MAYA_EXE_PATH = r"C:\Program Files\Autodesk\Maya2018\bin\maya.exe"
+MAYA_EXE_PATH = r''
+#MAYA_DOLLAR_PATH = r"T:"
+MAYA_DOLLAR_PATH = r''
+#FILES_SAVING_PATH = r"D:\max_client"
+FILES_SAVING_PATH = r''
